f28c140af90ff344d2a969bd3c5adf93b77173c2
mattkeehan <matt.keehan@gmail.com>
2020-07-07 21:12:56 +0100

    added prettier and got fetch and display working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 import './App.css';
 
+const baseUrl = 'https://api.datamuse.com';
+
+const getWord = async (wordPart) => {
+  const url = `${baseUrl}/sug?s=${wordPart}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
 function App() {
+  const [wordToSearch, setWordToSearch] = useState('soup');
+  const [words, setWords] = useState(0);
+
+  const search = (wordToSearch) => {
+    async function fetchMyAPI() {
+      const resp = await getWord(wordToSearch);
+      setWords(resp);
+    }
+
+    fetchMyAPI();
+  };
+
+  useEffect(() => search(wordToSearch), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-3">
+      <Jumbotron>
+        <h1 className="header">Guess the completed word</h1>
+      </Jumbotron>
+
+      {/* <div>{JSON.stringify(words)}</div> */}
+
+      <ListGroup>
+        {words.length &&
+          words.map((item, key) => (
+            <ListGroup.Item key={key}>{item.word}</ListGroup.Item>
+          ))}
+      </ListGroup>
+
+      <p>Your word is {wordToSearch}</p>
+
+      <div>
+        <input
+          type="text"
+          value={wordToSearch}
+          onChange={(e) => setWordToSearch(e.target.value)}
+        />
+      </div>
+
+      <Button variant="flat" size="xxl" onClick={() => search(wordToSearch)}>
+        Click me
+      </Button>
+    </Container>
   );
 }
 
